89ac534d7d9501dfafe546026ccd4be6f2da2200
huangjj27 <huangjj.27@qq.com>
2019-04-25 21:57:44 +0800

    code formated

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
 mod player;
-mod util;
 mod remux;
+mod util;
 
 // use.
-use wasm_bindgen::prelude::*;
 use crate::util::events::EventEmitter;
 use crate::util::events::Listener;
-
+use wasm_bindgen::prelude::*;
 
 // types.
 pub type Events<T> = EventEmitter<Listener<T>>;
 
-
 // 创建播放器
 // pub fn createPlayer (mediaDataSource: MediaDataSource, config: Config) {
-    
+
 // }
